887924f98a223a6de8458036585a14c0a5a30afa
cesar santana <cjsseng@gmail.com>
2021-10-11 10:50:24 -0300

    initial commit

---
+++ model.c
@@ -0,0 +1,4 @@
+typedef struct{
+    int posicoes [10][10];
+    int valores_das_posicoes_da_nova_geracao[100];
+} Mundo;
---
+++ view.c
@@ -0,0 +1,26 @@
+void mostraMundo(int posicoes[10][10]){
+ int i, j;
+ 
+    for(i = 0; i < 11; i++){
+        if (i == 0){
+            printf("  ");
+            for (j = 0; j < 10; j++){
+                if(j != 9){
+                    printf("%d ", j);
+                } else{
+                    printf("%d\n", j);
+                }
+            }
+        }else{
+            printf("%d ", (i-1));
+            for (j = 0; j < 10; j++){
+                if(j != 9){
+                    (posicoes[i-1][j] == 0 ? printf("- ") : printf("* ")); 
+                } else{
+                    (posicoes[i-1][j] == 0 ? printf("-") : printf("*")); 
+                    printf("\n");
+                }
+            }
+        }
+    }
+}
