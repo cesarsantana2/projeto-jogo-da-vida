9f199a9839dacd54eb090a2021ebb3a6728d23bf
cesar santana <cjsseng@gmail.com>
2021-10-23 18:20:02 -0300

    criando logica do jogo

--- controller.c
+++ controller.c
@@ -4,37 +4,26 @@
 // Declarando a assinatura das funcoes
 void determinar_tamanho_da_matriz();
 void fluxo_do_jogo(int opcao);
+void adicionar_vida_em_uma_posicao();
+int verifica_se_a_posicao_estah_contida(int x, int y);
+void controla_jogo();
 
 //CONTADORES GLOBAIS
-
-int tamanho_da_matriz_quadrada = 0;
+int tamanho_da_matriz_quadrada;
 int quantidade_pecas_vivas = 0;
 int quantidade_vizinhos_mortos = 0;
+int quantidade_pecas_vivas_proxima_geracao = 0;
+int quantidade_vizinhos_mortos_proxima_geracao = 0;
 
 
 void entrypoint(){
 
-    int opcao, tamanho_da_matriz;
+    int opcao_menu_principal = -1;
 
-    opcao = menu_principal();
-    fluxo_do_jogo(opcao);
+    while(opcao_menu_principal != 0){
 
-    //TO-DO 
-    //exibir_mundo();
-
-
-}
-
-void inicia_posicoes_das_listas_do_jogo(){
-
-    int i;
-
-    for (i = 0, i < 500; i++){
-        
-        pecas_vivas[i] = 0;
-        vizinhos_mortos = 0;
-        pecas_vivas_proxima_geracao[i] = 0;
-        vizinhos_mortos_proxima_geracao[i] = 0;
+        opcao_menu_principal = menu_principal();
+        fluxo_do_jogo(opcao_menu_principal);
     }
 }
 
@@ -42,20 +31,58 @@ void fluxo_do_jogo(int opcao){
 
     switch(opcao){
         
-        case 1: determinar_tamanho_da_matriz();
+        case 1: controla_jogo();
                 break;
 
         default:
             break;
     }
+}   
 
-}     
+void fluxo_de_geracoes(int opcao){
+
+    switch(opcao){
+  
+
+        case 1: adicionar_vida_em_uma_posicao();
+                exibir_mundo(tamanho_da_matriz_quadrada);
+                break;
+
+        default: break;
+    }
+}
+
+void controla_jogo(){
+
+    int opcao_menu_de_geracoes = -1;
+
+    determinar_tamanho_da_matriz();
+
+    while(opcao_menu_de_geracoes != 0){
+
+        opcao_menu_de_geracoes =  menu_fluxo_de_geracoes();
+        fluxo_de_geracoes(opcao_menu_de_geracoes);
+    }
+}
+
+
+
+void controla_geracoes(){}
 
 int verifica_se_a_posicao_estah_contida(int x, int y){
 
-    int i = 0;
+    int i;
 
+    for (i = 0; i < quantidade_pecas_vivas; i++){
+
+        if(pecas_vivas.pos[i].linha == x && pecas_vivas.pos[i].coluna == y){
+            return 1;
+        }
+    }
+
+    return 0;
 }
+
 
 void determinar_tamanho_da_matriz(){
 
@@ -64,6 +91,26 @@ void determinar_tamanho_da_matriz(){
 
 }
 
-void selecionar_posicoes_para_atribuir_vida(){
+void adicionar_vida_em_uma_posicao(){
+
+    int lin, coln;
+    int opcao = -1;
+   
+    printf("Por favor selecione linha e coluna para adicionar vida ou 0 para sair | [Ex.de input: 4,5]:  ");
+     
+
+
+    scanf("%d,%d", &lin,&coln);
+
+    pecas_vivas.pos[quantidade_pecas_vivas].linha = lin;
+    pecas_vivas.pos[quantidade_pecas_vivas].coluna = coln;
+
+    quantidade_pecas_vivas++;
+}
+
+void identificar_vizinhos_de_uma_peca_viva(){
+
+    
+
 
 }
--- view.c
+++ view.c
@@ -1,3 +1,5 @@
+int verifica_se_a_posicao_estah_contida(int x, int y);
+
 int menu_principal(){
 
     int opcao;
@@ -6,10 +8,10 @@ int menu_principal(){
     printf("Grupo CCAJ\n");
     printf("=========================\n");
     printf("1 - Inicializar a Matriz\n");
-    printf("2 - Recuperar e salvar uma geracao inicial\n");
+    printf("2 - Salvar uma geracao\n");
     printf("3 - Recuperar uma geracao inicial cadastrada\n");
     printf("4 - INICIAR O PROCESSO\n");
-    printf("5 - Mostra a Matriz final apos o processo\n");
+    printf("5 - Exibir ultima geracao da ultima simluacao \n");
     printf("6 - Limpar o cadastro de 7 geracoes iniciais\n");
     printf("0 - Sair\n");
 
@@ -18,7 +20,58 @@ int menu_principal(){
     return opcao;
 }
 
-void exibir_mundo(){
+int menu_fluxo_de_geracoes(){
 
+    int opcao;
+
+    printf("\n\nSelecione uma Opcao:\n\n");
+
+    printf("1 - Adicionar vida em uma posicao\n");
+    printf("2 - Remover vida de uma posicao\n");
+    printf("3 - Iniciar Simulacao de Geracoes\n");
+    printf("0 - Voltar para o menu principal\n");
+
+    scanf("%d", &opcao);
+
+    return opcao;
+}
+
+int menu_de_fim_de_jogo(){
+
+    int opcao;
+
+    printf("\n\nFIM DA SIMULACAO\n\n");
     
+    printf("Selecione uma opcao para prosseguir:\n\n");
+
+    printf("1 - Voltar para o inicio da simulacao\n");
+    printf("2 - Comecar novo jogo\n");
+    printf("3 - Carregar estrutura pre construida\n");
+    printf("0 - Sair\n");
+
+    scanf("%d", &opcao);
+
+    return opcao;
+}
+
+void exibir_mundo(int tam){
+
+    int i, j;
+    int cont = tam;
+
+
+    printf("\n\nPRINTANDO A MATRIZ\n\n");
+
+    for (i = 0; i < cont; i++){
+        printf("%d ", i);
+        for (j = 0; j < cont; j++){
+
+            if(verifica_se_a_posicao_estah_contida(i, j)){
+                printf("* ");
+            }else{
+                printf("- ");
+            }
+        }
+        printf("\n");
+    }    
 }
